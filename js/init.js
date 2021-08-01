659a6b819cff51c0d5e670faa23c7bfd43ebb9f7
EnriqueLozadaIc41 <88256882+EnriqueLozadaIc41@users.noreply.github.com>
2021-07-31 20:02:18 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -8,11 +8,11 @@
  * servidor. */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyCfMMR4bFjJgfECWOsx6iRCHPZTeDQ9GBQ",
-    authDomain: "bdfalumnos41.firebaseapp.com",
-    projectId: "bdfalumnos41",
-    storageBucket: "bdfalumnos41.appspot.com",
-    messagingSenderId: "463512573259",
-    appId: "1:463512573259:web:2531082c01b911490b04ce",
-    measurementId: "G-DXY3ZYMKY4"
+    apiKey: "AIzaSyCe9sYBZlT1tSoU4b0ak95DVoPH6xF5zsI",
+    authDomain: "evaluacionlelf-ea04a.firebaseapp.com",
+    projectId: "evaluacionlelf-ea04a",
+    storageBucket: "evaluacionlelf-ea04a.appspot.com",
+    messagingSenderId: "196768144659",
+    appId: "1:196768144659:web:fd3dc2aea65c51e1e84ee6",
+    measurementId: "G-C63Y9851HQ"
 });
